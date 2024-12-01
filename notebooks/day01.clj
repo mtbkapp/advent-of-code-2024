dd4f3adf25891c8464d5505e07c6e550adb54338
Jason Kapp <mtbkapp@gmail.com>
2024-12-01 12:17:12 -0700

    Day 01 - Solutions

--- notebooks/day01.clj
+++ notebooks/day01.clj
@@ -1,5 +1,25 @@
 ;# Day 1
-(ns day01)
+(ns day01
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
 
-(reduce + (range 2000))
+(def data
+  (let [by-line (->> (slurp (io/resource "day01.txt"))
+                     (string/split-lines)
+                     (map (fn [line]
+                            (map parse-long (string/split line #"\s+" )))))]
+    {:left (map first data)
+     :right (map second data)}))
 
+(def part1
+  (->> (map (comp abs -)
+            (sort (:left data))
+            (sort (:right data)))
+       (reduce +)))
+
+(def part2
+  (let [freqs (frequencies (:right data))]
+    (transduce (map #(* % (get freqs % 0)))
+               +
+               0
+               (:left data))))
